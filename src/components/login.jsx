4844813b826823c93435a2e67e3c07ffd9f4efb4
viniferraria <vferraria10@gmail.com>
2020-09-21 12:17:35 -0300

    ajuste no fetch

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -36,7 +36,7 @@ class Login extends Component {
                                 <div className="col-4 offset-4">
                                     <div className="input-group mb-2">
                                         <div className="input-group-prepend">
-                                            <div className="input-group-text"><b>Email</b></div>
+                                            <div className="input-group-text"><b>E-mail</b></div>
                                         </div>
                                     <input id="email" name="email" className="form-control" type="text" placeholder="Digite seu e-mail" value={this.state.usuario.email} onChange={this.handleEmail}/>
                                     </div>
@@ -123,7 +123,7 @@ class Login extends Component {
     handleLogin = async () => {
         // let baseUrl = "https://webarber-back.herokuapp.com"
         let baseUrl = 'localhost:8080';
-
+        
         console.log(baseUrl);
         if(this.state.loginPage === false){
             this.setState({loginPage: true});
