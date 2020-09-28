3ff18f7ad34cd6d1e263ce3f2129c693cbb40698
viniferraria <vferraria10@gmail.com>
2020-09-28 17:57:50 -0300

    added router

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import Register from './register';
 import Log from './log';
 import webarber from "../images/webarber.png";
+import './home.css';
 
 export default function Home() {
     // Hooks
@@ -13,29 +14,30 @@ export default function Home() {
     }
 
     return (
-        <div className="conteiner d-flex justify-content-center">
+        <div>
             <div className="row">
                 <h1> webarber</h1>
             </div>
-            <div className='row'>
-                <div className="card text-center">
-                    <div className="card-header">
-                        <img className="card-img-top-center" src={webarber} alt="webarber logo" style={{ width: "310px", height: "90px" }}></img>
-                    </div>
-                    <div className="card-body">
-                        <div className="row">
-                            <div className="col md-col-6">
-                                <h5 className={tab ? "active" : "disabled "}>Login</h5>
-                            </div>
-                            <div className="col md-col-6">
-                                <h5 className={tab ? "active" : "disabled "}>Cadastre-se</h5>
-                            </div>
+            <div className="conteiner d-flex justify-content-center">
+                <div className='row'>
+                    <div className="card text-center">
+                        <div className="card-header">
+                            <img className="card-img-top-center" src={webarber} alt="webarber logo" style={{ width: "310px", height: "90px" }}></img>
                         </div>
-                        <br/>
-                        <br/>
-                        <div className="conteiner d-flex justify-content-center">
+                        <div className="card-body">
                             <div className="row">
-                                { (!tab)? <Log/> : <Register/> }
+                                <div className="col md-col-6">
+                                    <h5 className={!tab ? "active" : "disabled "}>Login</h5>
+                                </div>
+                                <div className="col md-col-6">
+                                    <h5 className={tab ? "active" : "disabled "}>Cadastre-se</h5>
+                                </div>
+                            </div>
+                            <br/>
+                            <div className="conteiner d-flex justify-content-center">
+                                <div className="row">
+                                    { (!tab)? <Log/> : <Register/> }
+                                </div>
                             </div>
                         </div>
                     </div>
